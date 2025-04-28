7afb0525d393889af744e86e0f953e2e3a3bdc44
heroglad <078bct071.sagar@pcampus.edu.np>
2025-04-28 12:35:55 +0545

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -196,7 +196,7 @@ const Contact = () => {
                         
                             <div className='mt-5'>
                                 <h2 className="text-2xl">Send an E-mail</h2>
-                                <p className="text-gray-400">deltaquants.info@gmail.com</p>
+                                <p className="text-gray-400">betaquants.info@gmail.com</p>
                             </div>
                        
                         </div>
